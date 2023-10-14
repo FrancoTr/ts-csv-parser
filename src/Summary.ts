2fe4575cb8f706843894a62236957b36647a26ff
Franco Traverso <franco-t20@outlook.com>
2023-10-13 23:14:30 -0300

    Fix typo on interface

--- src/Summary.ts
+++ src/Summary.ts
@@ -4,12 +4,12 @@ export interface Analyzer {
   run(matches: MatchData[]): string;
 }
 
-export interface outputTarget {
+export interface OutputTarget {
   print(report: string): void;
 }
 
 export class Summary {
-  constructor(public analyzer: Analyzer, public outputTarger: outputTarget) {}
+  constructor(public analyzer: Analyzer, public outputTarger: OutputTarget) {}
 
   buildAndPrintReport(matches: MatchData[]): void {
     const output = this.analyzer.run(matches);
